c5536278cc4f335106bc54cc8b395d231861445e
kate-latushkina <59828320+kate-latushkina@users.noreply.github.com>
2020-03-02 16:30:07 +0200

    feat: add correct solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members == null || members == undefined || members == Number || !Array.isArray(members)) {
+    return false;
+  }
+  else {
+    let teamArr = [];
+    let result;
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] == 'string') {
+        teamArr.push(members[i].toUpperCase().trim().slice(0, 1));
+        teamArr.sort();
+        result = teamArr.join('');
+      }
+
+    }
+    return result;
+  }
 };
