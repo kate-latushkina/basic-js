2b44369e3c84afc4cf0ce5a23ebe19f298433462
kate-latushkina <59828320+kate-latushkina@users.noreply.github.com>
2020-03-03 16:43:51 +0200

    feat: add correct solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,30 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    let getArray = [];
+    if (!Array.isArray(arr)) {
+        throw new Error('Error. This argument is not an Array.');
+    };
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '--discard-next') {
+            i++;
+        }
+        else if (arr[i] == '--discard-prev') {
+            if (i != 0) {
+                getArray.splice(getArray.length - 1, 1);
+            }
+        }
+        else if (arr[i] == '--double-next') {
+            if (i + 1 != arr.length) {
+                getArray.push(arr[i + 1]);
+            }
+        }
+        else if (arr[i] == '--double-prev') {
+            if (i != 0) {
+                getArray.push(arr[i - 1]);
+            }
+        }
+        else {
+            getArray.push(arr[i]);
+        }
+    }
+    return getArray;
+}
